16ce6a23f0bde88378e8f407fa820c1301eb0d74
xiondisc <xiondisc@eskelson.com>
2021-06-18 11:16:22 -0600

    update

--- TurtleTest/turtleTest.lua
+++ TurtleTest/turtleTest.lua
@@ -2,6 +2,13 @@ print "Routine initializing."
 
 for i=0,50,1
 do
+  local fuel = turtle.getFuelLevel()
+  if (fuel < 10) then
+    turtle.refuel()  
+  end
+
+  print("Fuel: " . fuel)
+
   turtle.digDown()
   turtle.down()
   turtle.dig()
